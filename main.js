df0e1d5d17b9e6f43e34e6ab3a35f9029b702087
priteshkhot <priteshkhot12@gmail.com>
2025-11-24 21:07:05 +0530

    Update main.js
    updated button functionality and dark/light mode changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const keys = document.querySelectorAll(".keys"); // makes a list of all 8 keys
 const about = document.querySelector(".about"); // About button to give info aabout which keys to press
 
 const numKeys = keys.length;
-const infoKeys = ["A", "S", "D", "F", "J", "K", "L", ";"]; // Keys that can be pressed
+const infoKeys = ["A", "S", "D", "F", "H", "J", "K", "L"]; // Keys that can be pressed
 var aboutFlag = false; // Will use this to to check if keys are displayed
 
 // just for testing
@@ -11,16 +11,17 @@ function selectKey() {
     console.log(keyname);
 }
 
-// for each works like a for loop;
+// foreach works like a for loop;
 keys.forEach(key => {
     key.addEventListener("click", function(event) {
         var keyname = event.target.getAttribute("id") // I have named the audio files the same as their id
         var filename = "audio/" + keyname + "_.mp3";
         var audio = new Audio(filename);
-        audio.play()
+        audio.play();
     })
 });
 
+// About key/Toggle and see the keyboard keys to play xylophone
 about.addEventListener("click", () => {
     if (aboutFlag == false) {
         for (let i = 0; i < numKeys; i++) {
@@ -37,10 +38,21 @@ about.addEventListener("click", () => {
     
 })
 
+// Keyboard buttons to play xylophone
 document.addEventListener("keydown", function (event) {
-    makeSound(event.key)
+    btnToggle(event.key);
+    makeSound(event.key);
 })
 
+document.addEventListener("keyup", function (event) {
+    btnToggle(event.key);
+})
+
+// Corrresponding buttons will be shown active when you press through Keyboard
+function btnToggle(key) {
+    let activeKey = document.querySelector("." + key);
+    activeKey.classList.toggle('active');
+}
 
 function makeSound(key) {
     switch (key) {
@@ -64,22 +76,22 @@ function makeSound(key) {
             audio.play();
             break;
 
-        case "j":
+        case "h":
             var audio = new Audio('audio/key5_.mp3');
             audio.play();
             break;
 
-        case "k":
+        case "j":
             var audio = new Audio('audio/key6_.mp3');
             audio.play();
             break;
 
-        case "l":
+        case "k":
             var audio = new Audio('audio/key7_.mp3');
             audio.play();
             break;
 
-        case ";":
+        case "l":
             var audio = new Audio('audio/key8_.mp3');
             audio.play();
             break;
@@ -89,15 +101,25 @@ function makeSound(key) {
     }
 }
 
-// Dark mode/Lightmode
+// Dark mode/Light mode
 const darkBtn = document.querySelector("#darkmode")
 const lightBtn = document.querySelector("#lightmode")
 const mainBody = document.querySelector("body")
 
-darkBtn.addEventListener("click", ()=>
-    mainBody.setAttribute("class", "dark")
-)
+darkBtn.addEventListener("click", ()=> {
+    mainBody.setAttribute("class", "dark");
+    darkBtn.style.display = "none";
+    lightBtn.style.display = "inline-flex"
+    keys.forEach(key =>
+        key.classList.remove("add-border")
+    );
+});
 
-lightBtn.addEventListener("click", ()=>
-    mainBody.setAttribute("class", "light")
-)
+lightBtn.addEventListener("click", ()=> {
+    mainBody.setAttribute("class", "light");
+    lightBtn.style.display = "none";
+    darkBtn.style.display = "inline-flex";
+    keys.forEach(key =>
+        key.classList.add("add-border")
+    );
+});
